51b6a9017e07b92daf15efb72ca9886b60646913
zhou-20140 <234610382@qq.com>
2025-02-10 11:06:16 +0800

    fix bug

--- clm/start.py
+++ clm/start.py
@@ -1,7 +1,15 @@
 import mian
 import uex_tex_change as utc
+try:
+    mian.read_date()
+except FileNotFoundError:
+    print('dw error')
+    with open('clm\date\dw.txt','w') as f:
+        f.write('\n')
+    mian.add_fl('pt];pt]#error#;','tex','error,retrun')
+    mian.add_fl('pt]------------;[os];pt]------------;','tex','start')
+    mian.read_date()
 
-mian.read_date()
 print('read date complet')
 mian.runcode(mian.find('start.tex'))
 mian.lang='english'
