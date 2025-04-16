26780daa6e86298d061f6c84c30c2fb1d85e1fc7
zhou-20140 <234610382@qq.com>
2025-04-16 15:04:17 +0800

    update

--- clm/start.py
+++ clm/start.py
@@ -1,20 +1,30 @@
 import mian
 import uex_tex_change as utc
+import json
+try:
+    file=open("clm\date\config\config.json", 'r', encoding='utf-8')
+    config_data = json.load(file)
+
+except:
+    print(f"config error!")
+    mian.log.append('config error')
 try:
     mian.read_date()
 except FileNotFoundError:
-    print('dw error')
-    mian.log.append('dw error')
-    with open('clm\date\dw.txt','w') as f:
-        f.write('\n')
-    mian.add_fl('pt];pt]#error#;','tex','error,retrun')
-    mian.add_fl('pt]------------;[os];pt]------------;','tex','start')
-    mian.read_date()
+    if config_data['auto_fix_dw']:
+        print('dw error')
+        mian.log.append('dw error')
+        with open('clm\date\dw.txt','w') as f:
+            f.write('\n')
+        mian.add_fl('pt];pt]#error#;','tex','error,retrun')
+        mian.add_fl('pt]------------;[os];pt]------------;','tex','start')
+        mian.read_date()
 
 print('read date complet')
 mian.log.append('read date complet')
 mian.runcode(mian.find('start.tex'))
-mian.lang='english'
+mian.lang=config_data['language']
+file.close()
 
 while mian.cts:
     utc.context_run(input('<=> '))
