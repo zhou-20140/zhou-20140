8fd6b918aeb16b80abb2d62a3f0ab5114af483ce
zhou-20140 <234610382@qq.com>
2025-04-17 13:13:23 +0800

    update

--- clm/mian.py
+++ clm/mian.py
@@ -53,6 +53,8 @@ def save_date():
         file=open('clm\\date\\'+i.name+'.txt','w')
         file.write(i.context)
     file.close()
+    with open('clm\\date\\logs\\log1','a+') as file:
+        file.write('\n'.join(log))
 
 def read_date():
     ed=open('clm\\date\\dw.txt','r')
